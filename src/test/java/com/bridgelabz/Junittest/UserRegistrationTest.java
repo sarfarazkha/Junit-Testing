bf07b5525fac0c82fff26772648e49404fb93e1b
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-25 23:53:43 +0530

    user need to enter predefine Rule4 has exactly 1 special char in password

--- src/test/java/com/bridgelabz/Junittest/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/Junittest/UserRegistrationTest.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.Junittest;
+
 import com.bridgelabz.Junit.UserRegistration;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,28 +10,28 @@ public class UserRegistrationTest {
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateFirstName("Praju");
+        boolean result = userRegistration.validateFirstName("sarfaraz");
         Assert.assertTrue(result);
     }
 
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateLastName("Hanasi");
+        boolean result = userRegistration.validateLastName("khan");
         Assert.assertTrue(result);
     }
 
     @Test
     public void givenEmail_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateEmail("praju@123");
+        boolean result = userRegistration.validateEmail("sarfaraz@123");
         Assert.assertTrue(result);
     }
 
     @Test
     public void givenMobileNumber_WhenNotMatched_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateMobileNumber("8765343900");
+        boolean result = userRegistration.validateMobileNumber("8765342540");
         Assert.assertTrue(result);
     }
 
@@ -38,14 +39,14 @@ public class UserRegistrationTest {
     @Test
     public void givenRule1_WhenNotMatched_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateRule1("paayyvvtt6@gbvh8");
+        boolean result = userRegistration.validateRule1("saayyvvtt6@gbvh8");
         Assert.assertTrue(result);
     }
 
     @Test
     public void givenRule2_WhenNotMatched_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateRule2("paayyvvtt6@gbvh8");
+        boolean result = userRegistration.validateRule2("saayyvvtt6@gbvh8");
         Assert.assertTrue(result);
     }
     @Test
@@ -57,8 +58,9 @@ public class UserRegistrationTest {
     @Test
     public void givenRule4_WhenNotMatched_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateRule4("paayyvvtt6@gbvh8");
+        boolean result = userRegistration.validateRule4("saayyvvtt6@gbvh8");
         Assert.assertTrue(result);
     }
 }
+
 
