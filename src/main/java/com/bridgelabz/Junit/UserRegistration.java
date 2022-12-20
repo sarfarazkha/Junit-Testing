a76e2ed65d0dc4984a22e15803f6cf8fa3113321
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-20 22:45:26 +0530

    Added: user need to enter predefine Rule1 min 8 digit password

--- src/main/java/com/bridgelabz/Junit/UserRegistration.java
+++ src/main/java/com/bridgelabz/Junit/UserRegistration.java
@@ -33,6 +33,6 @@ public class UserRegistration {
     public boolean validateMobileNumber(String phoneNumber) {
         pattern = Pattern.compile(MOBILE_PATTERN);
         matcher = pattern.matcher(phoneNumber);
-        return matcher.matches();
+        return matcher.matches(); 
     }
 }
